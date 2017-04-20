8d6395fd6548948ee8805cdfa75233c3f38e5bbf
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2017-04-20 16:17:35 -0300

    changed timeouts to quasi zero.

--- src/luarpc.lua
+++ src/luarpc.lua
@@ -39,7 +39,7 @@ end
 
 -- in : table with parameters
 -- out: string in the form '{parameter1, parameter2, ...}'
-function M.marshall(t)
+function M.marshall_table(t)
 	local s = '{'
 	for i,v in ipairs(t) do
 		if     (type(v) == "string") then s = s.."'"..v.."'"
@@ -54,22 +54,30 @@ end
 -- in : string with encoded parameter list
 -- out: table as array with parameters in order
 function M.unmarshall(s)
-	return loadstring(s)()
+	return loadstring(s)
 end
 
 -- name: function name
 -- args: table with parameters
 -- ret : string in the form 'return{`name`={args}}
 function M.marshall_call(name, args)
-	return 'return{'..name..'='..M.marshall(args)
+	return 'return{'..name..'='..M.marshall_table(args)..'}'
 end
 
 -- in : string with encoded call
 -- out: function name, table with parameters
 function M.unmarshall_call(s)
-	for call,params in pairs(M.unmarshall(s)) do
+	for call,params in pairs(M.unmarshall(s)()) do
 		return call, params
 	end
+end
+
+function M.marshall_ret(t)
+	return 'return'..M.marshall_table(t)
+end
+
+function M.unmarshall_ret(t)
+	return M.unmarshall(t)()
 end
 
 function M.waitIncoming()
@@ -79,7 +87,7 @@ function M.waitIncoming()
 			local client = conn
 			if M.servers[conn] then
 				client = assert(conn:accept())
-				client:settimeout(1)
+				client:settimeout(0.01) -- no timeout
 				M.servers[client] = M.servers[conn]
 			end
 
@@ -87,14 +95,11 @@ function M.waitIncoming()
 				local m,e = client:receive()
 				if m == nil or e == "timeout" then break end
 
-				-- TODO: process request
 				if M.servers[client] and m then
-					--print('processing request...')
-					local s = M.unmarshall(m)
 					local call, args = M.unmarshall_call(m)
+					-- TODO: validate that args conform with the IDL
 					local ret = {M.servers[client][call](unpack(args))}
-					--print(M.marshall(ret))
-					client:send('return'..M.marshall(ret)..'\n')
+					client:send(M.marshall_ret(ret)..'\n')
 				else client:send("__ERRORPC\n")
 				end
 			until e
@@ -124,20 +129,17 @@ function M.createProxy(ip, port, iface)
 				assert(type(params[pk]) == M.idltype2lua[idltype])
 			end
 			--marshall
-			local msg = "return{"..k..'='..M.marshall(params)..'}\n'
-			--print(msg)
+			local msg = "return{"..k..'='..M.marshall_table(params)..'}\n'
+			local msg = M.marshall_call(k, params)..'\n'
 
 			-- send
 			self.conn:send(msg)
 
 			-- receive
 			local s,m = self.conn:receive()
-			--print(s,e)
-
-			-- TODO: validate returned values
-			-- TODO: unmarshall
-			-- TODO: return (ret, out, inout)
-			return unpack(M.unmarshall(s))
+			local ret = M.unmarshall_ret(s)
+			-- TODO: validate that ret values conform with IDL
+			return unpack(ret)
 		end
 	end
 
